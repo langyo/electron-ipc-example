c5403be22dec059498147b6d1044bf3f8117493a
伊欧 <langyo.china@gmail.com>
2021-07-29 01:44:30 +0800

    ✨ 现在支持三联线程打乒乓了。

--- client.ts
+++ client.ts
@@ -1,27 +1,53 @@
-import { join, isAbsolute, dirname, basename } from 'path';
-import { app, BrowserWindow, ipcMain } from 'electron';
+function log(...args) {
+  console.log(`[主进程] ${new Date(Date.now()).toLocaleTimeString()}`, ...args);
+}
 
 let win: BrowserWindow;
-app.whenReady().then(() => {
-  let count = 0;
-  setInterval(() => {
-    console.log(
-      `${new Date(
-        Date.now()
-      ).toLocaleTimeString()} 与主进程交换消息的分发速度：${count}`
-    );
-    count = 0;
-  }, 1000);
+let count = 0;
+setInterval(() => {
+  log('分发速度：', count);
+  count = 0;
+}, 1000);
 
-  ipcMain.on('asynchronous-message', (event, raw) => {
+import { join } from 'path';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { createServer, Socket } from 'net';
+
+let connection: Socket;
+let outsideIpcServer = createServer((connect) => {
+  connection = connect;
+  connect.setEncoding('utf8');
+  connect.on('error', (err) => {
+    log('对外 IPC 通道发生错误：', err);
+    connect.end();
+  });
+  connect.on('close', () => {
+    log('对外 IPC 通道已关闭');
+  });
+  connect.on('data', (data: string) => {
+    const { type } = JSON.parse(data);
+    switch (type) {
+      case 'pong':
+        count += 1;
+        win.webContents.send(
+          'asynchronous-reply',
+          JSON.stringify({ type: 'pong' })
+        );
+        break;
+    }
+  });
+}).listen(join('\\\\?\\pipe', '\\electronIPCDemo'));
+outsideIpcServer.on('error', (err) => {
+  log('对外 IPC 通道发生错误：', err);
+});
+
+app.whenReady().then(() => {
+  ipcMain.on('asynchronous-message', (_event, raw) => {
     const { type } = JSON.parse(raw);
 
     switch (type) {
       case 'ping':
-        count += 1;
-        event.reply('asynchronous-reply', {
-          type: 'pong',
-        });
+        connection.write(JSON.stringify({ type: 'ping' }));
         break;
     }
   });
@@ -37,4 +63,14 @@ app.whenReady().then(() => {
   });
 
   win.loadFile(join(__dirname, './index.html'));
+});
+
+import { spawn } from 'child_process';
+let childProcess = spawn('node', [join(__dirname, './server.js')], {
+  stdio: 'inherit',
+  detached: true,
+  shell: true,
+});
+process.on('exit', () => {
+  childProcess.kill();
 });
