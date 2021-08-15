4562c2208451d0412531645d8210d66d1d9d281e
伊欧 <langyo.china@gmail.com>
2021-08-16 01:32:43 +0800

    🐛 部分修复了交换过程中数据长度不对齐的问题。 但由于交换速度过快，导致有时候不能稳定进行数据交换。 要发送的数据如果太短（尤其是小于等于 2 字节时），一定会出现这种问题（推测是由于数据包开头的文本长度容易被误判为数据本体造成的）。

--- IPCDemo.CS/Program.cs
+++ IPCDemo.CS/Program.cs
@@ -6,65 +6,61 @@ using System.Text;
 
 namespace IPCDemo.CS
 {
-    public class PipeClient
+  public class PipeClient
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var pipeClient =
-                new NamedPipeClientStream(".", "electronIPCDemo",
-                    PipeDirection.InOut, PipeOptions.None,
-                    TokenImpersonationLevel.Impersonation);
+      var pipeClient =
+          new NamedPipeClientStream(".", "electronIPCDemo",
+              PipeDirection.InOut, PipeOptions.None,
+              TokenImpersonationLevel.Impersonation);
 
-            Console.WriteLine("正在连接到渲染进程...\n");
-            pipeClient.Connect();
+      Console.WriteLine("正在连接到渲染进程...\n");
+      pipeClient.Connect();
 
-            var ss = new StreamString(pipeClient);
-            for (var s = ss.ReadString(); ; s = ss.ReadString())
-            {
-                Console.Write(s);
-                ss.WriteString(s);
-            }
-            // pipeClient.Close();
-        }
+      var ss = new StreamString(pipeClient);
+      for (var s = ss.ReadString(); ; s = ss.ReadString())
+      {
+        Console.WriteLine(s);
+        ss.WriteString(s);
+      }
+      // pipeClient.Close();
+    }
+  }
+
+  // Defines the data protocol for reading and writing strings on our stream.
+  public class StreamString
+  {
+    private Stream ioStream;
+    private UnicodeEncoding streamEncoding;
+
+    public StreamString(Stream ioStream)
+    {
+      this.ioStream = ioStream;
+      streamEncoding = new UnicodeEncoding();
     }
 
-    // Defines the data protocol for reading and writing strings on our stream.
-    public class StreamString
+    public string ReadString()
     {
-        private Stream ioStream;
-        private UnicodeEncoding streamEncoding;
+      int len;
+      len = ioStream.ReadByte() * 256;
+      len += ioStream.ReadByte();
+      var inBuffer = new byte[len];
+      ioStream.Read(inBuffer, 0, len);
 
-        public StreamString(Stream ioStream)
-        {
-            this.ioStream = ioStream;
-            streamEncoding = new UnicodeEncoding();
-        }
-
-        public string ReadString()
-        {
-            int len;
-            len = ioStream.ReadByte() * 256;
-            len += ioStream.ReadByte();
-            var inBuffer = new byte[len];
-            ioStream.Read(inBuffer, 0, len);
-
-            return streamEncoding.GetString(inBuffer);
-        }
-
-        public int WriteString(string outString)
-        {
-            byte[] outBuffer = streamEncoding.GetBytes(outString);
-            int len = outBuffer.Length;
-            if (len > ushort.MaxValue)
-            {
-                len = ushort.MaxValue;
-            }
-            ioStream.WriteByte((byte)(len / 256));
-            ioStream.WriteByte((byte)(len & 255));
-            ioStream.Write(outBuffer, 0, len);
-            ioStream.Flush();
-
-            return outBuffer.Length + 2;
-        }
+      return streamEncoding.GetString(inBuffer);
     }
+
+    public int WriteString(string outString)
+    {
+      byte[] outBuffer = streamEncoding.GetBytes(outString);
+      int len = outBuffer.Length;
+      ioStream.WriteByte((byte)(len / 256));
+      ioStream.WriteByte((byte)(len % 256));
+      ioStream.Write(outBuffer, 0, len);
+      ioStream.Flush();
+
+      return outBuffer.Length + 2;
+    }
+  }
 }
--- IPCDemo.Electron/client.ts
+++ IPCDemo.Electron/client.ts
@@ -17,31 +17,49 @@ let outsideIpcServer = createServer((connect) => {
     console.log('对外 IPC 通道已关闭');
   });
   connect.on('data', (data: Buffer) => {
-    console.log(data.slice(2).toString('utf8'));
-    win?.webContents.send(
-      'asynchronous-reply',
-      'ping'
-    );
+    const num = data.slice(2).toString('utf8');
+    console.log('即将接收:', data[0] * 256 + data[1], num);
+    win?.webContents.send('asynchronous-reply', num);
   });
-  let buffer = Buffer.from('ping');
-  let length = buffer.byteLength;
-  buffer = Buffer.concat([Buffer.from([Math.ceil(length / 256), length % 256]), buffer]);
+  let buffer = Buffer.from('114514');
+  buffer = Buffer.concat([
+    Buffer.from([Math.floor(buffer.byteLength / 256), buffer.byteLength % 256]),
+    buffer,
+  ]);
+  console.log(
+    '即将写出:',
+    buffer[0] * 256 + buffer[1],
+    buffer.slice(2).toString('utf8')
+  );
   connection?.write(buffer);
 }).listen(join('\\\\?\\pipe', '\\electronIPCDemo'));
 
 import { execFile } from 'child_process';
 console.log('尝试启动外部进程...');
-let childProcess = execFile(join(process.cwd(), '../IPCDemo.CS/bin/Debug/net5.0/IPCDemo.CS.exe'), err => {
-  if (err) {
-    console.error(err);
+let childProcess = execFile(
+  join(process.cwd(), '../IPCDemo.CS/bin/Debug/net5.0/IPCDemo.CS.exe'),
+  (err) => {
+    if (err) {
+      console.error(err);
+    }
   }
-});
+);
 
 app.whenReady().then(() => {
-  ipcMain.on('asynchronous-message', (_event, _raw) => {
-    let buffer = Buffer.from('ping');
-    let length = buffer.byteLength;
-    buffer = Buffer.concat([Buffer.from([Math.ceil(length / 256), length % 256]), buffer]);
+  ipcMain.on('asynchronous-message', (_event, raw) => {
+    let buffer = Buffer.from(`${raw}`);
+    buffer = Buffer.concat([
+      Buffer.from([
+        Math.floor(buffer.byteLength / 256),
+        buffer.byteLength % 256,
+      ]),
+      buffer,
+    ]);
+    console.log(
+      '即将写出:',
+      buffer[0] * 256 + buffer[1],
+      buffer.slice(2).toString('utf8')
+    );
     connection?.write(buffer);
   });
 
