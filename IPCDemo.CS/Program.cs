cf125334a2d0022268c9e2ec61821630b2787559
lanyi <lanyi@ra3.moe>
2021-08-19 08:43:28 +0200

    格式化 C# 代码

--- IPCDemo.CS/Program.cs
+++ IPCDemo.CS/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.IO;
 using System.IO.Pipes;
 using System.Security.Principal;
@@ -6,76 +8,65 @@ using System.Text.Json;
 
 namespace IPCDemo.CS
 {
-  public class IMsg
-  {
-    public string caller { get; set; }
-    public string callee { get; set; }
-    public string[] args { get; set; }
-  }
+  [SuppressMessage("Code Quality", "CA1819:Properties should not return arrays")]
+  public record Msg(string Caller, string Callee, string[] Args) { }
 
-  public class PipeClient
+  public static class PipeClient
   {
     public static void Main(string[] args)
     {
       // 根据传入的命令行参数，连接到对应的命名管道
-      var socketClient =
+      using var socketClient =
           new NamedPipeClientStream(".", args[0],
               PipeDirection.InOut, PipeOptions.None,
               TokenImpersonationLevel.Impersonation);
       socketClient.Connect();
-      var socketClientStream = new Stream(socketClient);
 
       // 创建自己的命名管道，并将命名管道的名称交给对方
-      string socketServerAddress = Guid.NewGuid().ToString();
-      var socketServer = new NamedPipeServerStream(socketServerAddress, PipeDirection.InOut, 1);
-      var socketServerStream = new Stream(socketServer);
-      socketClientStream.Write(new IMsg
-      {
-        caller = "cs-" + Guid.NewGuid().ToString(),
-        callee = "$shakehand",
-        args = new string[1] { socketServerAddress }
-      });
+      var socketServerAddress = Guid.NewGuid().ToString();
+      using var socketServer = new NamedPipeServerStream(socketServerAddress, PipeDirection.InOut, 1);
+      socketClient.Write(new Msg
+      (
+          Caller: "cs-" + Guid.NewGuid().ToString(),
+          Callee: "$shakehand",
+          Args: new[] { socketServerAddress }
+      ));
       socketServer.WaitForConnection();
 
-      for (var s = socketServerStream.Read(); socketClient.IsConnected; s = socketServerStream.Read())
+      for (var s = socketServer.Read(); socketClient.IsConnected; s = socketServer.Read())
       {
-        int num = Convert.ToInt32(s.args[0]);
+        var num = int.Parse(s.Args[0], CultureInfo.InvariantCulture);
         num += 1;
-        socketClientStream.Write(new IMsg
-        {
-          caller = s.caller,
-          callee = s.callee,
-          args = new string[1] { Convert.ToString(num) }
-        });
+        socketClient.Write(new Msg
+        (
+            Caller: s.Caller,
+            Callee: s.Callee,
+            Args: new[] { num.ToString(CultureInfo.InvariantCulture) }
+        ));
       }
-
-      socketClient.Close();
-      socketServer.Close();
     }
   }
 
-  public class Stream
+  internal static class StreamExtensions
   {
-    private System.IO.Stream ioStream;
-
-    public Stream(System.IO.Stream ioStream)
+    private static readonly JsonSerializerOptions s_jsonOptions = new()
     {
-      this.ioStream = ioStream;
-    }
+      PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
 
-    public IMsg Read()
+    public static Msg Read(this Stream ioStream)
     {
-      int len = ioStream.ReadByte() * 256 + ioStream.ReadByte();
+      var len = ioStream.ReadByte() * 256 + ioStream.ReadByte();
       var inBuffer = new byte[len];
       ioStream.Read(inBuffer, 0, len);
 
-      return JsonSerializer.Deserialize<IMsg>(inBuffer);
+      return JsonSerializer.Deserialize<Msg>(inBuffer, s_jsonOptions)!;
     }
 
-    public int Write(IMsg msg)
+    public static int Write(this Stream ioStream, Msg msg)
     {
-      byte[] outBuffer = JsonSerializer.SerializeToUtf8Bytes(msg);
-      int len = outBuffer.Length;
+      var outBuffer = JsonSerializer.SerializeToUtf8Bytes(msg, s_jsonOptions);
+      var len = outBuffer.Length;
       ioStream.WriteByte((byte)(len / 256));
       ioStream.WriteByte((byte)(len % 256));
       ioStream.Write(outBuffer, 0, len);
